d1f4b5a86038dcfd6c2c8d020067c36b72e3fa47
cabalex <31020729+cabalex@users.noreply.github.com>
2025-02-27 11:26:28 -0800

    Code cleanup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-from flask import Flask, render_template
 from api import create_app
 
 app = create_app()
-
-@app.route("/")
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
